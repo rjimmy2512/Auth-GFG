b4bbfc17deaccd83b014564a4554f8a1e796b009
James Ruhashyankiko <jruhashyankiko@activated.io>
2022-09-02 12:28:51 -0700

    Added my project

---
+++ server/passport.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+
+const passport = require("passport");
+const dotenv=require('dotenv');
+
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/auth/google/callback",
+            scope: ["profile", "email"]
+        },
+        function (accessToken, refreshToken, profile, callback) {
+            callback(null,profile)
+            console.log(profile);
+        }
+    )
+);
+
+passport.use(
+    new FacebookStrategy(
+        {
+            clientID: process.env.FACEBOOK_APP_ID,
+            clientSecret: process.env.FACEBOOK_APP_SECRET,
+            callbackURL: "/auth/facebook/callback",
+            scope: ["profile", "email"],
+        },
+        function (accessToken, refreshToken, profile, callback) {
+            callback(null, profile);
+            console.log(email);
+        }
+    )
+);
+
+passport.use(
+    new GithubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: "/auth/github/callback",
+            scope: ["user:email"],
+        },
+        function (accessToken, refreshToken, profile, callback) {
+           callback(null, profile);
+        }
+    )
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportSetup = require("./passport");
+const authRoute = require("./routes/auth");
+const cookieSession = require("cookie-session");
+const app = express();
+
+
+app.use(
+    cookieSession({
+        name:"session",
+        keys: ["oauth-gfg"],
+        maxAge: 24 * 60 * 60 * 100,
+    })
+);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(
+    cors({
+        origin:"http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true,
+    })
+);
+
+app.use("/auth", authRoute);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server up on port ${port}...`));
